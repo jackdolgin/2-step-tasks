2e92feb582cabb7e2c25040f231c7e137bb9bc2e
Jack Dolgin <jack.dolgin@duke.edu>
2020-11-11 02:51:53 -0600

    Update behavioral_summaries.R
    Now all the raw data from Daw's and Da Silva's tasks is imported in R and properly formatted

--- behavioral_summaries.R
+++ behavioral_summaries.R
@@ -9,8 +9,26 @@ magic_carpet_path <- here("Feher da Silva & Hare 2020", "results", "magic_carpet
 
 
 
-#daw_data <- readMat(file.path(daw_path, "data.mat"))
+daw_data_raw <- readMat(file.path(daw_path, "data.mat"))                            # takes several minutes to run this line
 
+daw_data_plucked <- daw_data_raw %>%
+  pluck(1) %>%
+  rbindlist() 
+
+daw_col_names = c("s", "stim_1_left", "stim_1_right", "rt_1", "choice1",
+                     "stim_2_left", "stim_2_right", "rt_2", "choice2", "win",
+                     "state2", "common", "score", "practice", "ps1a1", "ps1a2",
+                     "ps2a1", "ps2a2", "trial", "unsure")
+
+daw_data <- daw_data_plucked %>%
+  mutate(running_count = row_number() / (nrow(daw_data_plucked) / 20),
+         count_ceil = ceiling(running_count),
+         soon_col_name = daw_col_names[count_ceil]) %>%
+  group_by(count_ceil) %>%
+  mutate(across(running_count, ~ rank(.))) %>%
+  ungroup() %>%
+  select(-count_ceil) %>%
+  pivot_wider(names_from = soon_col_name, values_from = V1)
 
 spaceship_data <- spaceship_path %>%
   dir_ls(regexp = "[[:digit:]].csv") %>%
